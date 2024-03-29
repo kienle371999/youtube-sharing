4b2cebd3827a54f2113400b962734d84b961dad3
kienle371999 <kienle371999@gmail.com>
2024-03-29 15:10:51 +0700

    [YS] Add the first api for BE

--- be/db/seeds.rb
+++ be/db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.delete_all
+Video.delete_all
+
+User.create([
+    { name: 'John Doe', email: 'john@example.com', password: '123456' },
+    { name: 'Jane Smith', email: 'jane@example.com', password: '123456' },
+])
+
+Video.create([
+    { link: 'https://www.youtube.com/watch?v=wMCtDXrJA2k&ab_channel=VTVGi%E1%BA%A3iTr%C3%ADOfficial', description: 'Description 1', like_number: 10, dislike_number: 2, user_id: 1 },
+    { link: 'https://www.youtube.com/watch?v=wMCtDXrJA2k&ab_channel=VTVGi%E1%BA%A3iTr%C3%ADOfficial', description: 'Description 2', like_number: 5, dislike_number: 1, user_id: 2 },
+    { link: 'https://www.youtube.com/watch?v=wMCtDXrJA2k&ab_channel=VTVGi%E1%BA%A3iTr%C3%ADOfficial', description: 'Description 3', like_number: 15, dislike_number: 3, user_id: 1 },
+])
