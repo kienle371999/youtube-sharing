97b63247e13137db7ceffc089bad75e5f2ae2d29
kienle371999 <kienle371999@gmail.com>
2024-03-29 15:13:35 +0700

    [YS] Update code

---
+++ be/db/migrate/20240329074228_create_videos.rb
@@ -0,0 +1,13 @@
+class CreateVideos < ActiveRecord::Migration[7.1]
+  def change
+    create_table :videos do |t|
+      t.string :link
+      t.text :description
+      t.integer :like_number
+      t.integer :dislike_number
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
